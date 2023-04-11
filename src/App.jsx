84eccf46ffc16c5968a4fd4d4e60a8b1eae0a0ae
Ashik <ashik23402@gmail.com>
2023-04-11 19:48:01 +0600

    Initial setup

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      
+     <p className='text-red-600'>Ashik broh</p>
+     
+    </div>
+  )
+}
+
+export default App
