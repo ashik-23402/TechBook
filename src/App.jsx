cc3b362148be4564e41ebc1e2212d0aa65ed62dc
Ashik <ashik23402@gmail.com>
2023-05-01 19:27:12 +0600

    initial project setup for Techbook

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,4 @@
-import { useState } from 'react'
-import Nav from './Components/Nav'
-import Signup from './Components/Pages/Signup'
-import Login from './Components/Pages/Login'
-import Home from './Components/Pages/Home'
-import QuizPage from './Components/Pages/QuizPage'
-import { BrowserRouter as Router,Route,Routes } from 'react-router-dom'
-import {AuthProvider} from './Context/AuthContext'
-import PrivateRoute from './Components/PrivateRoute'
-import PublicRoute from './Components/PublicRoute'
-import ScorePage from './Components/Pages/ScorePage'
+import { useState } from 'react';
 
 
 function App() {
@@ -16,58 +6,7 @@ function App() {
 
   return (
     <div>
-
-      <Router>
-     <AuthProvider>
-      <Nav/>
-
-       <div className="h-44
-    w-44 bg-[rgb(255,255,255,0.3)] absolute -right-0 mt-3 
-    rounded-[50%]"></div>
-
-    <div className="h-44
-    w-44 bg-[rgb(255,255,255,0.3)] absolute left-1 bottom-2
-    rounded-[50%] m-2"></div>
-
-
-      <Routes>
-
-        
-        <Route exact path='/' Component={Home}/>
-
-
-        <Route exact path='/signup' element={<PublicRoute/>}>
-            <Route exact path='/signup' element={<Signup/>}/> 
-        </Route> 
-
-
-        <Route exact path='/login' element={<PublicRoute/>}>
-            <Route exact path='/login' element={<Login/>}/> 
-        </Route> 
-
-
-        <Route  path='/quiz' element={<PrivateRoute/>}>
-
-            <Route exact path='/quiz/:name' element={<QuizPage/>}/> 
-        
-        </Route>
-
-        
-        <Route  path='/score' element={<PrivateRoute/>}>
-
-            <Route exact path='/score/:score' element={<ScorePage/>}/> 
-        
-        </Route>
-
-
-      
-        
-        </Routes>
-
-        </AuthProvider>
-      </Router>
-
-     
+    hello world    
     </div>
   )
 }
